7c5fd6d180d6c4ed54a4e6e003164ba7320b13c3
hjhammerstrom <hannahjohammer@icloud.com>
2026-03-24 16:29:10 -0500

    Refactor feature_utils.py and update stock tickers

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -53,15 +53,15 @@ def extract_features_pair():
 
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
-    stk_tickers = ['AAPL', 'MPWR']
+    stk_tickers = ['ABNB', 'NKE']
     
     stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
 
-    Y = stk_data.loc[:, ('Adj Close', 'AAPL')]
-    Y.name = 'AAPL'
+    Y = stk_data.loc[:, ('Adj Close', 'ABNB')]
+    Y.name = 'ABNB'
 
-    X = stk_data.loc[:, ('Adj Close', 'MPWR')]
-    X.name = 'MPWR'
+    X = stk_data.loc[:, ('Adj Close', 'NKE')]
+    X.name = 'NKE'
 
     dataset = pd.concat([Y, X], axis=1).dropna()
     Y = dataset.loc[:, Y.name]
